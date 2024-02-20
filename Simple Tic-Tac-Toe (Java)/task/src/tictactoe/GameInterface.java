8997ef61edf5eee8433f402a43f59f8574173412
Jacob Hobbs <JacobRayHobbs@gmail.com>
2024-02-20 13:10:21 -0600

    First commit of 02/20
    Began new project, Simple Tic-Tac-Toe

---
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/GameInterface.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+public class GameInterface {
+
+    // Default constructor for GameInterface class
+    public GameInterface() {
+
+    }
+
+    public void start() {
+
+    }
+
+}
---
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -0,0 +1,8 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        GameInterface newGame = new GameInterface();
+        newGame.start();
+    }
+}
